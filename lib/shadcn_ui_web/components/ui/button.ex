b57ae08e8cdac9dd80716b53a056592ead1e420c
Emanoel Mendes <emanoelims@hotmail.com>
2024-03-29 10:36:11 -0300

    feat: add support to as attribute

--- lib/shadcn_ui_web/components/ui/button.ex
+++ lib/shadcn_ui_web/components/ui/button.ex
@@ -1,16 +1,23 @@
 defmodule ShadcnUIWeb.UI.Button do
   use Phoenix.Component
 
-  attr :variant, :string, default: "default"
-  attr :as_child, :boolean, default: false
-  attr :size, :string, default: "default"
+  attr :variant, :string,
+    default: "default",
+    values: ~W(default destructive outline secondary ghost link)
+
+  attr :size, :string,
+    default: "default",
+    values: ~W(default sm lg icon)
+
+  attr :as, :string, default: "button", examples: ~W(a button)
   attr :class, :string, default: nil
   attr :rest, :global
   slot :inner_block, required: true
 
   def button(assigns) do
     ~H"""
-    <button
+    <.dynamic_tag
+      name={@as}
       class={[
         "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
         render_button_variant(@variant),
@@ -20,38 +27,29 @@ defmodule ShadcnUIWeb.UI.Button do
       {@rest}
     >
       <%= render_slot(@inner_block) %>
-    </button>
+    </.dynamic_tag>
     """
   end
 
-  defp render_button_variant(variant_name) do
-    case variant_name do
-      "destructive" ->
-        "bg-destructive text-destructive-foreground hover:bg-destructive/90"
+  defp render_button_variant("default"),
+    do: "bg-primary text-primary-foreground hover:bg-primary/90"
 
-      "outline" ->
-        "border border-input bg-background hover:bg-accent hover:text-accent-foreground"
+  defp render_button_variant("destructive"),
+    do: "bg-destructive text-destructive-foreground hover:bg-destructive/90"
 
-      "secondary" ->
-        "bg-secondary text-secondary-foreground hover:bg-secondary/80"
+  defp render_button_variant("outline"),
+    do: "border border-input bg-background hover:bg-accent hover:text-accent-foreground"
 
-      "ghost" ->
-        "hover:bg-accent hover:text-accent-foreground"
+  defp render_button_variant("secondary"),
+    do: "bg-secondary text-secondary-foreground hover:bg-secondary/80"
 
-      "link" ->
-        "text-primary underline-offset-4 hover:underline"
+  defp render_button_variant("ghost"), do: "hover:bg-accent hover:text-accent-foreground"
+  defp render_button_variant("link"), do: "text-primary underline-offset-4 hover:underline"
 
-      _ ->
-        "bg-primary text-primary-foreground hover:bg-primary/90"
-    end
-  end
+  defp render_button_variant(_), do: render_button_variant("default")
 
-  def render_button_size(size) do
-    case size do
-      "sm" -> "h-9 rounded-md px-3"
-      "lg" -> "h-11 rounded-md px-8"
-      "icon" -> "h-10 w-10"
-      _ -> "h-10 px-4 py-2"
-    end
-  end
+  defp render_button_size("sm"), do: "h-9 rounded-md px-3"
+  defp render_button_size("lg"), do: "h-11 rounded-md px-8"
+  defp render_button_size("icon"), do: "h-10 w-10"
+  defp render_button_size(_), do: "h-10 px-4 py-2"
 end
